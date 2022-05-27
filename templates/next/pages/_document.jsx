2e4f8166e7280d833c806a9f0d7a7921f0f2169d
Anh Duy <duypa@spaceaa.com>
2022-05-27 21:57:48 +0700

    Fix[NextJS document]

--- templates/next/pages/_document.jsx
+++ templates/next/pages/_document.jsx
@@ -1,5 +1,4 @@
-import Document, { Html, Main, NextScript } from 'next/document';
-import Head from 'next/head';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
 	render() {
