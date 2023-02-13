b8cc1e43a88cedb0530b975b7d01f4d95b5be499
Nguyễn Văn Công <congnv3030@gmail.com>
2023-02-13 14:03:06 +0700

    update: react-vite javascript template

--- templates/react-vite/vite.config.js
+++ templates/react-vite/vite.config.js
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'url'
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    assetsDir: 'static',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'static/[hash].chunk.js',
+        assetFileNames: 'static/[hash].chunk.[ext]',
+      },
+    },
+    // manifest: true,
+  },
+});
